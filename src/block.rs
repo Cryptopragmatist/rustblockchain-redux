f0489970b130b41d697a23a16b15c2446dec515f
Cryptopragmatic <tristan.alberio@gmail.com>
2021-10-17 19:47:57 +0800

    blockchain lessons

---
+++ src/block.rs
@@ -0,0 +1,40 @@
+use std::fmt::{self, Debug,Formatter};
+
+use super::*; //imports files from the lib.rs
+
+pub struct Block { //struct for a block, public so that it can be accessed from other modules
+    pub index: u32,
+    pub timestamp: u128,
+    pub hash: BlockHash,
+    pub prev_block_hash: BlockHash,
+    pub nonce: u64,
+    pub payload: String,
+     
+}
+
+impl Debug for Block {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "Block[{}}]: {} at: {} with: {},",
+        &self.index,
+        &hex::encode(&self.hash),
+        &self.timestamp,
+        &self.payload,
+
+    
+    
+    )
+    }
+}
+impl Block { //implement functions corresponds to block struct
+    //implement a method for creating a  block
+    pub fn new(index: u32, timestamp: u128, prev_block_hash: BlockHash,nonce: u64, payload: String) -> Self {
+        Block {
+            index,
+            timestamp,
+            hash:vec![0;32], //array initializer, writing 32 element long array of zeros
+            prev_block_hash,
+            nonce,
+            payload,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use rustchain::Block;
+
 fn main() {
-    println!("Hello, world!");
+    let block = Block::new(0,0, vec![0;32],0 ,"Genesis block".to_owned());
+
+    println!("{:?}", &block);
 }
