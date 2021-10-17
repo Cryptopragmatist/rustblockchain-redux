98083593f7083bbf58fa713953ff414fc6524268
Cryptopragmatic <tristan.alberio@gmail.com>
2021-10-17 19:46:08 +0800

    blockchain lessons

---
+++ src/blockchainfundamentals.rs
@@ -0,0 +1,10 @@
+// blockchain = chronological, sequential list of blocks 
+/* Blockchain contains:
+Index: location of the block within the blockchain 
+Payload: data inside the block
+Timestamp: time within the blockchain 
+Nonce: special number used in mining (POW)
+Previous block hash: cryptographic fingerprint of the previous block 
+Hash: cryptographic fingerprint of all the data above put together*/
+
+//Hashing - a hash algorithm is a set of irreversible computations that can be performed on a data to generate a unique byte sequence
